c75c62852f98530358db78657267ab68af33ba79
Ben Harrison <bph958@uowmail.edu.au>
2018-10-22 14:20:25 +1100

    worked on the spawning of the zombie nests

--- GroupGame/Assets/Scripts/spawnEnvironment.cs
+++ GroupGame/Assets/Scripts/spawnEnvironment.cs
@@ -7,6 +7,7 @@ public class spawnEnvironment : MonoBehaviour {
     public GameObject prefab1;
     public GameObject prefab2;
     public GameObject prefab3;
+    public GameObject prefab4;
     public int randomX;
     public int randomY;
     public int minTree;
@@ -15,21 +16,31 @@ public class spawnEnvironment : MonoBehaviour {
     public int maxMetal;
     public int minStone;
     public int maxStone;
+    public int minNest;
+    public int maxNest;
 
 
     // Use this for initialization
     void Start () {
         for(int i = 0; i < Random.Range(minTree, maxTree); i++)
         {
-            Instantiate(prefab1, new Vector3(Random.Range(-50, randomX), Random.Range(-50, randomY), 0), Quaternion.identity);
+            Instantiate(prefab1, new Vector3(Random.Range(-100, randomX), Random.Range(-100, randomY), 0), Quaternion.identity);
         }
         for (int i = 0; i < Random.Range(minMetal, maxMetal); i++)
         {
-            Instantiate(prefab2, new Vector3(Random.Range(-50, randomX), Random.Range(-50, randomY), 0), Quaternion.identity);
+            Instantiate(prefab2, new Vector3(Random.Range(-100, randomX), Random.Range(-100, randomY), 0), Quaternion.identity);
         }
         for (int i = 0; i < Random.Range(minStone, maxStone); i++)
         {
-            Instantiate(prefab3, new Vector3(Random.Range(-50, randomX), Random.Range(-50, randomY), 0), Quaternion.identity);
+            Instantiate(prefab3, new Vector3(Random.Range(-100, randomX), Random.Range(-100, randomY), 0), Quaternion.identity);
+        }
+        for (int i = 0; i < (Random.Range(minNest, maxNest)/2); i++)
+        {
+            Instantiate(prefab4, new Vector3(Random.Range(-100, -75), Random.Range(-100, 100), 0), Quaternion.identity);
+        }
+        for (int i = 0; i < (Random.Range(minNest, maxNest) / 2); i++)
+        {
+            Instantiate(prefab4, new Vector3(Random.Range(100, 75), Random.Range(-100, 100), 0), Quaternion.identity);
         }
 
     }
